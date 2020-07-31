afc45782ff198259c1b3a0e0b7756e805f66dd7b
Mark Alston <malston@vmware.com>
2020-07-31 10:40:06 -0600

    Change resource name

--- terraform/modules/pas/nat.tf
+++ terraform/modules/pas/nat.tf
@@ -1,4 +1,4 @@
-resource "nsxt_nat_rule" "SNAT traffic for TAS" {
+resource "nsxt_nat_rule" "internal-to-external" {
   logical_router_id         = "${data.nsxt_logical_tier0_router.rtr0.id}"
   description               = "SNAT traffic for TAS rule provisioned by Terraform"
   display_name              = "SNAT traffic for TAS"
