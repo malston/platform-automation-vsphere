b9e8274f0832d4896d76b9a70b32a63b3283cd27
Mark Alston <malston@vmware.com>
2020-09-24 21:34:50 -0600

    Add task to upgrade cluster

---
+++ tasks/upgrade-cluster/task.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+function login_pks_k8s_cluster() {
+	local cluster="${1}"
+	local password="${2}"
+
+	printf "Logging into k8s cluster (%s)..." "${cluster}"
+	echo "${password}" | pks get-credentials "${cluster}" > /dev/null 2>&1
+
+	return $?
+}
+
+function main() {
+    local cluster="${1}"
+    local password="${2}"
+
+    if ! login_pks_k8s_cluster "${cluster}" "${password}"; then
+        echo
+        printf "Failed to login to cluster '%s'" "${cluster}" 
+        exit 1
+    fi
+
+    pks upgrade-cluster "${cluster}" --non-interactive > upgrade.log 2>&1 &
+
+    cluster_uuid=$(pks cluster "${cluster}" --json | jq -r .uuid)
+
+    task_id="0"
+    while [[ $(bosh -d "service-instance_${cluster_uuid}" tasks --column=state | head -3 | awk '{print $1}') =~ processing ]]; do
+        current_task_id="$(bosh -d "service-instance_${cluster_uuid}" tasks --column=id | head -3 | awk '{print $1}')"
+        if [[ "$current_task_id" != "$task_id" ]]; then
+            printf "Logging output from task '%s'" "$current_task_id"
+            bosh task "$task_id" >> task.log 2>&1
+        fi
+        task_id="$current_task_id"
+    done
+    # tail task.log
+}
+
+cluster="${1:-$CLUSTER}"
+password="${2:-$PKS_PASSWORD}"
+
+if [[ -z "${cluster}" ]]; then
+    echo "PKS cluster is required"
+    exit 1
+fi
+
+if [[ -z "${password}" ]]; then
+    echo "PKS admin password is required"
+    exit 1
+fi
+
+main "$cluster" "$password"
