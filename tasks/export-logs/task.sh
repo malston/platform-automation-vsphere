1ed932b4a0bb719a0c65b41c4ecd23f9964903c8
Mark Alston <malston@vmware.com>
2020-10-01 09:25:24 -0600

    Use sed instead of envsubst

--- tasks/export-logs/task.sh
+++ tasks/export-logs/task.sh
@@ -10,6 +10,6 @@ export timestamp
 # '$timestamp' must be a literal, because envsubst uses it as a filter
 # this allows us to avoid accidentally interpolating anything else.
 # shellcheck disable=SC2016
-output_file_name="$(echo "${EXPORT_FILE_NAME}" | envsubst '$timestamp')"
+output_file_name="$(echo "${EXPORT_FILE_NAME}" | sed -e s/'$timestamp'/$timestamp/g)"
 
 /opt/tasks/show-logs > logs/"${output_file_name}"
