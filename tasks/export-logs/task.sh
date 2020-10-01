44e4e8f0c05593011c889230631e377053d62b16
Mark Alston <malston@vmware.com>
2020-10-01 09:12:16 -0600

    Add task to export logs

---
+++ tasks/export-logs/task.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eux
+
+mkdir -p logs
+
+timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
+export timestamp
+
+# '$timestamp' must be a literal, because envsubst uses it as a filter
+# this allows us to avoid accidentally interpolating anything else.
+# shellcheck disable=SC2016
+output_file_name="$(echo "${EXPORT_FILE_NAME}" | envsubst '$timestamp')"
+
+/opt/tasks/show-logs > logs/"${output_file_name}"
