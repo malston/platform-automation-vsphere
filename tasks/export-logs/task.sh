9fcad02bcb809a8d38ee6ebaa5b76d5dec6d2b0a
Mark Alston <malston@vmware.com>
2020-10-01 09:51:12 -0600

    Append timestamp to end of filename

--- tasks/export-logs/task.sh
+++ tasks/export-logs/task.sh
@@ -5,11 +5,5 @@ set -eux
 mkdir -p logs
 
 timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
-export timestamp
 
-# '$timestamp' must be a literal, because envsubst uses it as a filter
-# this allows us to avoid accidentally interpolating anything else.
-# shellcheck disable=SC2016
-output_file_name="$(echo "${EXPORT_FILE_NAME}" | sed -e s/'$timestamp'/$timestamp/g)"
-
-/opt/tasks/show-logs > logs/"${output_file_name}"
+/opt/tasks/show-logs > logs/"${EXPORT_FILE_NAME}-${timestamp}"
