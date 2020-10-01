2e7f8f8c70591762251861e5c930c653ff574c91
Mark Alston <malston@vmware.com>
2020-10-01 09:54:20 -0600

    Refactor export-logs task

--- tasks/export-logs/task.sh
+++ tasks/export-logs/task.sh
@@ -6,4 +6,4 @@ mkdir -p logs
 
 timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')"
 
-/opt/tasks/show-logs > logs/"${EXPORT_FILE_NAME}-${timestamp}"
+/opt/tasks/show-logs > logs/"events-${JOB_NAME}-${timestamp}.log"
