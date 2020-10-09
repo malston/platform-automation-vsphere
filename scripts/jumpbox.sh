cda5e983dda0417021c562d1dd7047ce7564c036
Mark Alston <malston@vmware.com>
2020-10-09 11:45:46 -0600

    Polish jumpbox.sh

--- scripts/jumpbox.sh
+++ scripts/jumpbox.sh
@@ -1,22 +1,21 @@
 #!/bin/bash -e
 
-__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 OM_KEY=${1:-~/.ssh/om_rsa_key}
 
 PROJECT="${PROJECT_NAME:-platform-automation}"
 ENVIRONMENT="${ENVIRONMENT_NAME:-haas-439}"
+DOMAIN="pez.vmware.com"
 
 if [[ ! -f "${OM_KEY}" ]]; then
   if [[ -n $OPSMAN_SSH_KEY ]]; then
     printenv OPSMAN_SSH_KEY > "${OM_KEY}"
   else
     echo "Ops Manager ssh key not found... fetching it from credhub"
-    credhub get -n "/platform-automation/${ENVIRONMENT}/opsman_ssh_key" -k private_key > "${OM_KEY}"
+    credhub get -n "/${PROJECT}/${ENVIRONMENT}/opsman_ssh_key" -k private_key > "${OM_KEY}"
   fi
   chmod 600 "${OM_KEY}"
 fi
 
-SLOT_NUMBER="$(echo $ENVIRONMENT | cut -d - -f 2)"
-echo $GLOBAL_PASSWORD | pbcopy
-ssh -i "${OM_KEY}" "ubuntu@ubuntu-${SLOT_NUMBER}.${ENVIRONMENT_NAME}.pez.vmware.com"
+SLOT_NUMBER="$(echo "$ENVIRONMENT" | cut -d - -f 2)"
+echo "$GLOBAL_PASSWORD" | pbcopy
+ssh -i "${OM_KEY}" "ubuntu@ubuntu-${SLOT_NUMBER}.${ENVIRONMENT_NAME}.${DOMAIN}"
